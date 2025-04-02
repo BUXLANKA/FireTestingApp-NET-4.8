1dd4d31745bebee09c496b237fca40530efa537c
BUXLANKA <matwey.ishekov@gmail.com>
2025-04-02 15:58:33 +0300

    f1 - load

---
+++ Classes/QuestionObject.cs
@@ -0,0 +1,63 @@
+﻿using FireTestingApp.DataApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace FireTestingApp.Classes
+{
+    internal class QuestionObject
+    {
+        private void LoadQuestion(int questionId)
+        {
+            using (var context = new FireSafetyDBEntities())
+            {
+                // Загружаем вопрос
+                var question = context.Questions.FirstOrDefault(q => q.QuestionID == questionId);
+                if (question == null) return;
+
+                QuestionTextBlock.Text = question.Text; // Устанавливаем текст вопроса
+
+                // Загружаем ответы
+                var answers = context.Answers.Where(a => a.QuestionID == questionId).ToList();
+
+                AnswersPanel.Children.Clear(); // Очищаем прошлые ответы
+
+                foreach (var answer in answers)
+                {
+                    RadioButton radioButton = new RadioButton
+                    {
+                        Content = answer.Text, // Текст ответа
+                        Tag = answer.AnswerID, // ID ответа (для сохранения)
+                        FontSize = 14,
+                        Margin = new Thickness(5)
+                    };
+
+                    // Добавляем обработчик выбора ответа
+                    radioButton.Checked += AnswerSelected;
+
+                    AnswersPanel.Children.Add(radioButton);
+                }
+            }
+        }
+
+        // Сохранение выбора
+        private void AnswerSelected(object sender, RoutedEventArgs e)
+        {
+            RadioButton selectedRadio = sender as RadioButton;
+            if (selectedRadio != null)
+            {
+                int selectedAnswerId = (int)selectedRadio.Tag;
+                int questionId = currentQuestionId; // ID текущего вопроса
+
+                // Добавляем ответ в список
+                UserAnswers.RemoveAll(a => a.QuestionId == questionId);
+                UserAnswers.Add(new UserAnswer { QuestionId = questionId, SelectedAnswerId = selectedAnswerId });
+            }
+        }
+
+    }
+}
---
+++ Pages/MainTestPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using FireTestingApp.DataApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.Entity;  // Добавь это пространство имен
+
+namespace FireTestingApp.Pages
+{
+    public partial class MainTestPage : Page
+    {
+        private int currentQuestionIndex = 0;
+        private int score = 0;
+        private List<Question> questions;
+
+        public MainTestPage()
+        {
+            InitializeComponent();
+
+            // Подключаемся к базе данных и загружаем вопросы с ответами
+            using (var context = new FireSafetyDBEntities())
+            {
+                // Загружаем все вопросы с их ответами с использованием Include для жадной загрузки
+                var questionsFromDb = context.Questions.Include(q => q.Answers).ToList();
+
+                // Собираем всё в список
+                questions = questionsFromDb.Select(q => new Question
+                {
+                    QuestionID = q.QuestionID,
+                    QuestionText = q.QuestionText,
+                    Answers = q.Answers.ToList()  // Преобразуем ICollection<Answer> в List<Answer>
+                }).ToList();
+            }
+
+            // Проверяем, что есть вопросы
+            if (questions.Count == 0)
+            {
+                MessageBox.Show("Нет доступных вопросов.");
+                return;
+            }
+
+            // Загружаем первый вопрос
+            LoadQuestion();
+        }
+
+        private void LoadQuestion()
+        {
+            // Получаем текущий вопрос
+            var question = questions[currentQuestionIndex];
+
+            // Отображаем текст вопроса
+            QuestionTB.Text = question.QuestionText;
+
+            // Очищаем все элементы управления (если они есть)
+            Option1.Content = null;
+            Option2.Content = null;
+            Option3.Content = null;
+            Option4.Content = null;
+            Option5.Content = null;
+
+            // Добавляем ответы в RadioButton (если их меньше 5)
+            var answers = question.Answers.ToList();
+            if (answers.Count > 0) Option1.Content = answers[0].AnswerText;
+            if (answers.Count > 1) Option2.Content = answers[1].AnswerText;
+            if (answers.Count > 2) Option3.Content = answers[2].AnswerText;
+            if (answers.Count > 3) Option4.Content = answers[3].AnswerText;
+            if (answers.Count > 4) Option5.Content = answers[4].AnswerText;
+        }
+
+        // Пример перехода к следующему вопросу
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentQuestionIndex < questions.Count - 1)
+            {
+                currentQuestionIndex++;
+                LoadQuestion();
+            }
+            else
+            {
+                MessageBox.Show("Тест завершён!");
+            }
+        }
+    }
+}
