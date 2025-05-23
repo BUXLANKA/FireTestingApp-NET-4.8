67e64046600ff4dae9bad58e9db47778e3bb9358
BUXLANKA <matwey.ishekov@gmail.com>
2025-05-23 15:28:49 +0300

    галочка с согласием

--- Pages/UserPage.xaml.cs
+++ Pages/UserPage.xaml.cs
@@ -26,11 +26,23 @@ namespace FireTestingApp.Pages
             InitializeComponent();
 
             HelloLabel.Text = $"Добро пожаловать, {Session.UserFirstname} {Session.UserLastname}!";
+
+            StartTestButton.IsEnabled = false;
         }
 
         private void StartTestButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new MainTestPage());
+        }
+
+        private void TestAllowCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            StartTestButton.IsEnabled = true;
+        }
+
+        private void TestAllowCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            StartTestButton.IsEnabled = false;
         }
     }
 }
